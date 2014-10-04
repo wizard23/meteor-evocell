b0e3da54d77549486dd7493480bf1014c2859189
Philipp Tiefenbacher <wizards23@gmail.com>
2014-10-04 03:47:30 +0200

    got palette list with preview to work :)

--- client/paletteEditor.js
+++ client/paletteEditor.js
@@ -1,5 +1,19 @@
 Template.palettesList.helpers({
   palettes: function () {
     return Palettes.find();
+  },
+});
+
+Template.paletteItem.helpers({
+  colorItems: function() {
+    return _.map(this.colors, function(value, index){
+      return {color: value, index: index};
+    });
+  },
+});
+
+Template.paletteItemEntry.helpers({
+  htmlColor: function () {
+    return chroma(this.color).hex();
   }
 });
